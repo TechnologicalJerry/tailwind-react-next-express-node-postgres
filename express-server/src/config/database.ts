ed6fb4bb2dc0c7b73b8f9b5e1870794973059eb2
Rajat Verma <cserajatverma@gmail.com>
2026-01-28 18:48:38 +0530

    Export client from database config
    The postgres client is now exported from the database config file, allowing other modules to access the raw client instance if needed.

--- express-server/src/config/database.ts
+++ express-server/src/config/database.ts
@@ -6,7 +6,7 @@ import * as schema from '../db/schema';
 const connectionString = env.DATABASE_URL;
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
-const client = postgres(connectionString, { max: 1 });
+export const client = postgres(connectionString, { max: 1 });
 
 export const db = drizzle(client, { schema });
 
