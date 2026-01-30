40772f247d42aac942c2a797e5d711db03c1272f
Rajat Verma <cserajatverma@gmail.com>
2026-01-30 13:30:39 +0530

    Add basic auth service skeleton
    Introduces an authentication service with placeholder methods for login, signup, logout, and retrieving the current user. Implementation details to be added in future commits.

---
+++ react-client/src/services/auth.service.ts
@@ -0,0 +1,7 @@
+// Authentication service
+export const authService = {
+  login: async (email: string, password: string) => {},
+  signup: async (data: any) => {},
+  logout: async () => {},
+  getCurrentUser: async () => {},
+};
