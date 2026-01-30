e757dcce476ae6e3211893348a592001a56e2fe4
Rajat Verma <cserajatverma@gmail.com>
2026-01-30 13:30:43 +0530

    Add product service skeleton
    Introduces a new product service with placeholder async methods for CRUD operations. Implementation details to be added in future commits.

---
+++ react-client/src/services/product.service.ts
@@ -0,0 +1,8 @@
+// Product service
+export const productService = {
+  getProducts: async () => {},
+  getProduct: async (id: string) => {},
+  createProduct: async (data: any) => {},
+  updateProduct: async (id: string, data: any) => {},
+  deleteProduct: async (id: string) => {},
+};
